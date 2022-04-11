2325f1efa68bf19a8764a7a8338493f7b77df738
A7mad3bd <ahmadabd398@gmail.com>
2022-04-11 04:09:38 +0300

    Lab-18 Finished

--- src/main/java/com/example/codefellowship/Controller/ApplicationPostController.java
+++ src/main/java/com/example/codefellowship/Controller/ApplicationPostController.java
@@ -1,10 +1,11 @@
 package com.example.codefellowship.Controller;
 
-import com.example.codefellowship.Model.ApplicationPost;
-import com.example.codefellowship.Model.ApplicationUser;
+import com.example.codefellowship.Model.AppPost;
+import com.example.codefellowship.Model.AppUser;
 import com.example.codefellowship.Repository.PostRepository;
 import com.example.codefellowship.Repository.UseraRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -25,23 +26,26 @@ public class ApplicationPostController {
 
     @PostMapping("/post")
     public RedirectView addpost(Principal p, Model m, String body) {
-        ApplicationUser user = (ApplicationUser) appUserRepository.findByUsername(p.getName());
+        AppUser user = (AppUser) appUserRepository.findByUsername(p.getName());
         LocalDateTime createdAt = LocalDateTime.now();
-        ApplicationPost post = new ApplicationPost(body, createdAt, user);
+        AppPost post = new AppPost(body, createdAt, user);
         postRepository.save(post);
-        List<ApplicationPost> list = user.getPosts();
+        List<AppPost> list = user.getPosts();
         return new RedirectView("/posts");
+
+
     }
 
     @GetMapping("/post")
-    public String postpage() {
+    public String postpage(Model m, Principal p) {
         return "post";
     }
 
     @GetMapping("/posts")
-    public String postspage(Model m) {
+    public String postspage(Model m, Authentication auth) {
         List a;
         a = postRepository.findAll();
+        m.addAttribute("user1",auth.getPrincipal());
         m.addAttribute("posts", a);
         return "posts";
     }
--- src/main/java/com/example/codefellowship/Repository/PostRepository.java
+++ src/main/java/com/example/codefellowship/Repository/PostRepository.java
@@ -1,14 +1,12 @@
 package com.example.codefellowship.Repository;
 
-import com.example.codefellowship.Model.ApplicationPost;
-import com.example.codefellowship.Model.ApplicationUser;
+import com.example.codefellowship.Model.AppPost;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PostRepository extends JpaRepository<ApplicationPost, Long> {
-    public ApplicationPost findApplicationPostById(int id);
-    public ApplicationPost findById(long id);
-    public ApplicationPost findApplicationPostsById(int id);
+public interface PostRepository extends JpaRepository<AppPost, Long> {
+    public AppPost findById(long id);
+    public AppPost findApplicationPostsById(int id);
+    public AppPost findApplicationPostById(int id);
 }
